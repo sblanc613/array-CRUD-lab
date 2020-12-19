f9db5d15cf1c10426d835ceb0ac9b9ef7be4cf46
Samuel <sblanc613@gmail.com>
2020-12-18 23:54:50 -0500

    commit tree ea01a9c93af23a40f73c8b5635940782013adb74

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -1,35 +1,42 @@
 def create_an_empty_array
-  
+  []
 end
 
 def create_an_array
-  
+    create_an_array = ["Monday" , "Tuesday" , "Wednesday" , "Thursday"]
 end
 
 def add_element_to_end_of_array(array, element)
-  
+    add_element_to_end_of_array = ["Monday" , "Tuesday" , "Wednesday" , "Thursday"]
+    add_element_to_end_of_array << "arrays!"
 end
 
 def add_element_to_start_of_array(array, element)
-  
+    add_element_to_start_of_array = ["Monday" , "Tuesday" , "Wednesday" , "Thursday"]
+    add_element_to_start_of_array.unshift("wow")
 end
 
 def remove_element_from_end_of_array(array)
-  
+    array.pop
 end
 
 def remove_element_from_start_of_array(array)
-  
+    array.shift
 end
 
 def retrieve_element_from_index(array, index_number)
-  
+    retrieve_element_from_index = ["am" , "Tuesday" , "Wednesday"]
+    retrieve_element_from_index[0]
 end
 
 def retrieve_first_element_from_array(array)
+    retrieve_element_from_index = ["wow" , "Tuesday" , "Wednesday"]
+    retrieve_element_from_index[0]
   
 end
 
 def retrieve_last_element_from_array(array)
+    retrieve_element_from_index = ["wow" , "Tuesday" , "arrays!"]
+    retrieve_element_from_index[2]
   
 end
